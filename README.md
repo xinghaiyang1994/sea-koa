e3cc50f492ec7bb2a73d7603008d12d1680887be
xinghaiyang <xinghaiyang@daojia-inc.com>
2018-09-28 17:39:48 +0800

    feat couple

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # sea-koa
-需要配置 config/default.js
+基于 koa 构建的常用脚手架。
 
-```js
-module.exports = {
-    port: 3100,
-    database: {
-        host : '',     // ip 地址
-        user : '',
-        password : '',
-        database: '',        // 数据库名
-        port: 3306      // 默认端口是 3306 
+类型：
+* couple 分支为前后端耦合项目
+* api 分支为前后端分离项目
+
+## 启动步骤
+1. 下载安装依赖
+    ```shell
+    git clone https://github.com/xinghaiyang1994/sea-koa.git -b 分支名    
+    
+    cnpm i
+    ```
+2. 配置 config/default.js
+    ```js
+    module.exports = {
+        port: 3100,
+        database: {
+            host : '',     // ip 地址
+            user : '',
+            password : '',
+            database: '',        // 数据库名
+            port: 3306      // 默认端口是 3306 
+        }
     }
-}
-```
-
+    ```
+3. 启动
+    ```shell
+    node index.js
+    ```
 
 
--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const staticCache = require('koa-static-cache')
 const bodyParser = require('koa-bodyparser')
 const error = require('koa-error')
 const cors = require('koa2-cors')
+const helmet = require('koa-helmet')
+const log4js = require('log4js')
 
 const routes = require('./routes')
 const { port } = require('./config/default')
 
 const app = new Koa()
+const logger = log4js.getLogger()
+
+log4js.configure({
+    appenders: {
+        error: {       // 定义了名为 error 的 appender 
+            type: 'file',
+            filename: path.join(__dirname, './log/error.log')
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['error'],      // 使用名为 error 的 appender 
+            level: 'error'      
+        }
+    },
+    pm2: true       // 使用 pm2 启动项目
+})
 
 // 模板
 app.use(views(path.join(__dirname, './views'), {
@@ -25,15 +44,27 @@ app.use(bodyParser({
 }))
 
 // 支持跨域
-app.use(cors())
+app.use(cors({
+    'credentials': true
+}))
+
+// 提供安全 headers 
+app.use(helmet())
 
 // 错误处理
 app.use(error({
     engine: 'ejs',
     template: path.join(__dirname, './views/error.ejs')
 }))
+app.on('error', (err, ctx) => {
+    let errMsg = `${ctx.url} ${err}`
+    console.log(errMsg)
+    logger.error(errMsg)
+})
 
 // 路由
 routes(app)
 
-app.listen(port)
+app.listen(port, () => {
+    console.log('http://localhost:' + port)
+})
