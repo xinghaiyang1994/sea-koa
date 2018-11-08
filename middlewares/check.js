c4c443e07cc2be8d6c9dab90bd319704b7cfdb4c
xinghaiyang <xinghaiyang@daojia-inc.com>
2018-11-08 16:28:01 +0800

    delete 多余文件

--- middlewares/check.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    checkLogin: ctx => {
-        if (!ctx.session || !ctx.session.name) {
-            throw new Error('用户未登录，请登录后再试!')
-        }
-    }
-}
--- utils/tools.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    dealBody (option) {
-        return Object.assign({
-            code: 0,
-            message: '',
-            data: ''
-        }, option)
-    }
-}
